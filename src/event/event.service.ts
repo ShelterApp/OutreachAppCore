541cc68fa4965b9279307b05ac380ae835c6f503
ttphat <thanhphattran2197@gmail.com>
2022-05-28 15:29:02 +0700

    Should only show current or future events and not the past events

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,51 +1,61 @@
 import { BadRequestException, Injectable, Logger, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import moment from 'moment';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
-import { User } from 'src/users/schema/user.schema';
 import { SortParams } from 'src/utils/sort-params.dto';
 import { CreateEventDto } from './dto/create-event.dto';
 import { SearchParams } from './dto/search-params.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
-import { EventAttendees, EventAttendeesDocument } from './schema/event-attendees.schema';
-import { EventDocument, Event } from './schema/event.schema';
+import {
+  EventAttendees,
+  EventAttendeesDocument,
+} from './schema/event-attendees.schema';
+import { Event, EventDocument } from './schema/event.schema';
 
 @Injectable()
 export class EventService {
   constructor(
     @InjectModel(Event.name) private eventModel: SoftDeleteModel<EventDocument>,
-    @InjectModel(EventAttendees.name) private eventAttendeesModel: SoftDeleteModel<EventAttendeesDocument>,
+    @InjectModel(EventAttendees.name)
+    private eventAttendeesModel: SoftDeleteModel<EventAttendeesDocument>,
     private readonly logger: Logger,
-  ) { }
+  ) {}
   async create(createEventDto: CreateEventDto) {
     try {
       return await this.eventModel.create(createEventDto);
-    } catch(e) {
+    } catch (e) {
       this.logger.error(e);
       throw new UnprocessableEntityException('Error When Create Event');
     }
   }
 
-  async findAll(searchParams: SearchParams, sortParams: SortParams, skip = 0, limit = 20) {
+  async findAll(
+    searchParams: SearchParams,
+    sortParams: SortParams,
+    skip = 0,
+    limit = 20,
+  ) {
     let sort = this._buildSort(sortParams);
     const conditions = this._buildConditions(searchParams);
 
     const [result, total] = await Promise.all([
       this.eventModel
         .find(conditions)
-        .populate({ 
-          path: "createdBy",
-          select: 'name phone'
+        .populate({
+          path: 'createdBy',
+          select: 'name phone',
         })
         .sort([sort])
         .skip(skip)
         .limit(limit),
-        this.eventModel.countDocuments(conditions)
+      this.eventModel.countDocuments(conditions),
     ]);
     const myResult = [];
-    for(const res of result) {
-      const attendees = await this.eventAttendeesModel.find({eventId: res._id}).select('-_id userId userName userPhone userEmail attendedAt').lean();
-      
+    for (const res of result) {
+      const attendees = await this.eventAttendeesModel
+        .find({ eventId: res._id })
+        .select('-_id userId userName userPhone userEmail attendedAt')
+        .lean();
+
       myResult.push(this._setAttendees(res, attendees));
     }
 
@@ -53,14 +63,19 @@ export class EventService {
   }
 
   async findOne(id: string) {
-    const event = await (await (await this.eventModel.findById(id)).populate({
-      path: "createdBy",
-      select: 'name phone'
-    }));
+    const event = await await (
+      await this.eventModel.findById(id)
+    ).populate({
+      path: 'createdBy',
+      select: 'name phone',
+    });
     if (!event) {
       throw new NotFoundException('cannot_found_event');
     }
-    const attendees = await this.eventAttendeesModel.find({eventId: event._id}).select('-_id userId userName userPhone userEmail attendedAt').lean();
+    const attendees = await this.eventAttendeesModel
+      .find({ eventId: event._id })
+      .select('-_id userId userName userPhone userEmail attendedAt')
+      .lean();
     return this._setAttendees(event, attendees);
   }
 
@@ -80,7 +95,10 @@ export class EventService {
     if (event.countAttended < event.maxAttended) {
       // Check join event
       const userId = user._id || user.id;
-      const joined = await this.eventAttendeesModel.findOne({eventId: event._id, userId: userId});
+      const joined = await this.eventAttendeesModel.findOne({
+        eventId: event._id,
+        userId: userId,
+      });
       if (!joined) {
         const attend = await this.eventAttendeesModel.create({
           eventId: event._id,
@@ -88,7 +106,7 @@ export class EventService {
           userName: user.name,
           userPhone: user.phone,
           userEmail: user.email,
-          attendedAt: new Date()
+          attendedAt: new Date(),
         });
         event.countAttended = event.countAttended + 1;
         event.save();
@@ -108,9 +126,13 @@ export class EventService {
     }
     const userId = user._id || user.id;
     // Check join event
-    const joined = await this.eventAttendeesModel.findOneAndDelete({eventId: event._id, userId: userId});
+    const joined = await this.eventAttendeesModel.findOneAndDelete({
+      eventId: event._id,
+      userId: userId,
+    });
     if (joined) {
-      event.countAttended = event.countAttended > 0 ? event.countAttended - 1 : 0;
+      event.countAttended =
+        event.countAttended > 0 ? event.countAttended - 1 : 0;
       event.save();
       return true;
     } else {
@@ -119,7 +141,9 @@ export class EventService {
   }
 
   async update(id: string, updateEventDto: UpdateEventDto) {
-    const event = await this.eventModel.findByIdAndUpdate(id, updateEventDto).setOptions({ new: true });
+    const event = await this.eventModel
+      .findByIdAndUpdate(id, updateEventDto)
+      .setOptions({ new: true });
 
     if (!event) {
       throw new UnprocessableEntityException('error_when_update_camp');
@@ -130,18 +154,18 @@ export class EventService {
 
   async remove(id: string) {
     const filter = { _id: id };
-    const deleted = await this.eventModel.softDelete(filter);
+    const deleted = this.eventModel.softDelete(filter);
 
-    if(deleted.deleted <= 0) {
+    if (deleted.deleted <= 0) {
       throw new UnprocessableEntityException('error_when_delete_event');
     }
     return deleted.deleted > 0;
   }
 
   _setAttendees(event, attendes) {
-    const cloned = event.toObject({getters: true, versionKey: false});
-    cloned._id = cloned.id
-    delete cloned.id
+    const cloned = event.toObject({ getters: true, versionKey: false });
+    cloned._id = cloned.id;
+    delete cloned.id;
     cloned['attendes'] = attendes;
 
     return cloned;
@@ -149,22 +173,22 @@ export class EventService {
 
   _buildConditions(query) {
     type Conditions = {
-        title: string;
-        description: string;
-        location: any;
-        isDeleted: boolean;
-        $and: object[];
-    }
+      title: string;
+      description: string;
+      location: any;
+      isDeleted: boolean;
+      $and: object[];
+    };
     let conditions = {
-      isDeleted: false
+      isDeleted: false,
+      $and: [{ endDate: { $gte: Date.now() } }],
     } as Conditions;
     if (query.keyword) {
-      conditions.$and = [];
       conditions.$and.push({
-          $or: [
-              { title: { $regex: query.keyword, $options: "i" } },
-              { description: { $regex: query.keyword, $options: "i" } }
-          ]
+        $or: [
+          { title: { $regex: query.keyword, $options: 'i' } },
+          { description: { $regex: query.keyword, $options: 'i' } },
+        ],
       });
     }
 
@@ -172,20 +196,18 @@ export class EventService {
       conditions.location = {
         $near: {
           $geometry: {
-            type: "Point",
-            coordinates: [query.lng, query.lat]
+            type: 'Point',
+            coordinates: [query.lng, query.lat],
           },
-          $maxDistance: 50000
-        }
-      }
+          $maxDistance: 50000,
+        },
+      };
       if (query.maxDistance) {
         conditions.location.$near.$maxDistance = query.maxDistance;
       }
     }
 
-    
     return conditions ? conditions : {};
-
   }
 
   _buildSort(query) {
