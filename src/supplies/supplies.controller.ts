881b6c2db12d66e44b6c7963df7cafc24467d714
Thanh Phat <ttphat297@gmail.com>
2022-07-27 16:58:55 +0700

    update permission to get supplies

--- src/supplies/supplies.controller.ts
+++ src/supplies/supplies.controller.ts
@@ -18,62 +18,75 @@ import { Response } from 'express';
 @Roles(UserRole.Admin)
 @ApiTags('Supplies')
 @ApiBearerAuth()
-@UseInterceptors(new SanitizeMongooseModelInterceptor({excludeMongooseId: false, excludeMongooseV: true}))
+@UseInterceptors(
+  new SanitizeMongooseModelInterceptor({
+    excludeMongooseId: false,
+    excludeMongooseV: true,
+  }),
+)
 export class SuppliesController {
   constructor(private readonly suppliesService: SuppliesService) {}
 
   @Post()
   @ApiOperation({ summary: 'Create Supplies (Role: Admin)' })
-  @ApiOkResponse({status: 201, description: 'Supplies Object'})
+  @ApiOkResponse({ status: 201, description: 'Supplies Object' })
   async create(@Body() createSupplyDto: CreateSupplyDto) {
     return await this.suppliesService.create(createSupplyDto);
   }
 
   @Get()
-  @Roles(UserRole.Admin)
-  @Roles(UserRole.OrgLead)
-  @ApiOperation({ summary: 'Get list supplies (Role: Admin, OrgLead)' })
-  @ApiOkResponse({status: 200, description: 'Supplies list'})
-  async find(@Query() { skip, limit }: PaginationParams, @Query() searchParams: SearchParams) {
-    const [items, total] = await this.suppliesService.findAll(searchParams, skip, limit);
+  @ApiOperation({ summary: 'Get list supplies' })
+  @ApiOkResponse({ status: 200, description: 'Supplies list' })
+  async find(
+    @Query() { skip, limit }: PaginationParams,
+    @Query() searchParams: SearchParams,
+  ) {
+    const [items, total] = await this.suppliesService.findAll(
+      searchParams,
+      skip,
+      limit,
+    );
     return {
       items,
-      total
+      total,
     };
   }
 
   @Get(':id')
   @ApiParam({
-    name: 'id'
+    name: 'id',
   })
   @ApiOperation({ summary: 'Get supplies by id (Role: Admin)' })
-  @ApiOkResponse({status: 200, description: 'supplies object'})
+  @ApiOkResponse({ status: 200, description: 'supplies object' })
   async findOne(@Param() { id }: ParamsWithId) {
     return await this.suppliesService.findOne(id);
   }
 
   @Patch(':id')
   @ApiParam({
-    name: 'id'
+    name: 'id',
   })
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(UserRole.Admin)
   @ApiBearerAuth()
   @ApiOperation({ summary: 'update supplies (Role: Admin)' })
-  @ApiOkResponse({status: 200, description: 'supplies object'})
-  async update(@Param() { id }: ParamsWithId, @Body() updateSupplyDto: UpdateSupplyDto) {
+  @ApiOkResponse({ status: 200, description: 'supplies object' })
+  async update(
+    @Param() { id }: ParamsWithId,
+    @Body() updateSupplyDto: UpdateSupplyDto,
+  ) {
     return this.suppliesService.update(id, updateSupplyDto);
   }
 
   @Delete(':id')
   @ApiParam({
-    name: 'id'
+    name: 'id',
   })
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(UserRole.Admin)
   @ApiBearerAuth()
   @ApiOperation({ summary: 'delete supplies (Role: Admin)' })
-  @ApiOkResponse({status: 204, description: 'No content'})
+  @ApiOkResponse({ status: 204, description: 'No content' })
   async remove(@Param() { id }: ParamsWithId, @Res() res: Response) {
     await this.suppliesService.remove(id);
 
